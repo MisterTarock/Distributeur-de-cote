daf01e635c3938814d85641959f3ff4d29da28cf
MisterTarock <victor_pb@hotmail.fr>
2016-10-18 17:05:02 +0200

    hello paolo

--- Distributeur de cotes/Distributeur de cotes/Program.cs
+++ Distributeur de cotes/Distributeur de cotes/Program.cs
@@ -10,44 +10,53 @@ namespace Distributeur_de_cotes
     {
         static void Main(string[] args)
         {
-            //To define the students
-            Student Puissant = new Student("Puissant", "Victor");
-            Student Petit = new Student("Petit", "Jon");
 
-            //To define the teachers
-            Teacher Combe = new Teacher("Combefis","Sebastien", 2500); //the salary that is a int should not set between ""
-            Teacher Fle = new Teacher("Flemal", "Clemence", 4500);
-            Teacher Lur = new Teacher("Lurquin", "Quentin", 10000);
+            //Encore à améliorer....
 
-            //To define the differnete activity
-            Activity COO = new Activity("Conception oriente objet","COO", 3, Combe); // the same for the teacher call by is var
-            Activity EN = new Activity("Electo Numérique", "EN", 4, Fle);
-            Activity PI = new Activity("Projet Info", "PO", 10, Lur);
+           
+             //To define the students
+             Student Puissant = new Student("Puissant", "Victor");
+             Student Petit = new Student("Petit", "Jon");
 
-            //To define the appreciations
-            Appreciation PuissantCOO = new Appreciation(COO,"X");
-            Appreciation PuissantEN = new Appreciation(EN, "");
-            Appreciation PuissantPI = new Appreciation(PI, "B");
+             //To define the teachers
+             Teacher Combe = new Teacher("Combefis","Sebastien", 2500); //the salary that is a int should not set between ""
+             Teacher Fle = new Teacher("Flemal", "Clemence", 4500);
+             Teacher Lur = new Teacher("Lurquin", "Quentin", 10000);
 
-            Appreciation PetitCOO = new Appreciation(COO, "X");
+             //To define the differnete activity
+             Activity COO = new Activity("Conception oriente objet","COO", 3, Combe); // the same for the teacher call by is var
+             Activity EN = new Activity("Electo Numérique", "EN", 4, Fle);
+             Activity PI = new Activity("Projet Info", "PO", 10, Lur);
 
-            //The bulletin from Puissant
-            Puissant.Add(PuissantCOO);
-            Puissant.Add(PuissantEN);
-            Puissant.Add(PuissantPI);
+             //To define the appreciations
+             Appreciation PuissantCOO = new Appreciation(COO,"X");
+             Appreciation PuissantEN = new Appreciation(EN, "");
+             Appreciation PuissantPI = new Appreciation(PI, "TB");
 
-            //The bulletin from Petit
-            Petit.Add(PetitCOO);
+             Appreciation PetitCOO = new Appreciation(COO, "X");
 
-            Console.WriteLine(Puissant.Bulletin());
-            Console.WriteLine(Petit.Bulletin());
+             //The bulletin from Puissant
+             Puissant.Add(PuissantCOO);
+             Puissant.Add(PuissantEN);
+             Puissant.Add(PuissantPI);
 
-            Console.ReadKey();
+             //The bulletin from Petit
+             Petit.Add(PetitCOO);
+
+             Console.WriteLine(Puissant.Bulletin());
+             Console.WriteLine(Puissant.Average());
+
+             Console.WriteLine(Petit.Bulletin());
+             Console.WriteLine(Petit.Average());
+
+             Console.ReadKey(); 
+
+             
 
 
 
 
-            
+
 
         }
     }
--- Distributeur de cotes/Distributeur de cotes/Student.cs
+++ Distributeur de cotes/Distributeur de cotes/Student.cs
@@ -27,6 +27,7 @@ namespace Distributeur_de_cotes
         }
 
         //to make the average of all the eval
+        //right now the calcul of the average is false....
         public double Average()
         {
             var sum = 0;
@@ -39,35 +40,35 @@ namespace Distributeur_de_cotes
 
         //to make a Bulletin
         public string Bulletin()
-        {
-            // The evaluations can be assigned to any activity and are not sorted
-            // We first need to sort them by activity to be able to take an average
-            // For this we create a dictionary (hashmap) where the key is an activity and the value is a tuple of two integers
-            // representing the running sum of the scores for that activity and the number of evaluations.
+        {  /*We create the Bulletin that is a dictionary where the key is an activity (with a code and a name) 
+            and the value is a tuple of two int (the ects and the note)
+            */
+            
+           
             // For each activity we check if it already exists in the dictionary, if it does we update the sum and number
             // otherwise we add the activity
-            Dictionary<Activity, Tuple<int, int>> gradesPerActivity = new Dictionary<Activity, Tuple<int, int>>();
+            Dictionary<Activity, Tuple<int, int>> cotesforActivity = new Dictionary<Activity, Tuple<int, int>>();
 
-            foreach (var g in this.cours)
+            foreach (var point in this.cours)
             {
                 // We try to access a given key in the dictionary, if it does not exist it will throw an exception
                 // and we know we have to add it to the dictionary.
                 try
                 {
-                    Tuple<int, int> t = gradesPerActivity[g.Activity];
-                    gradesPerActivity[g.Activity] = new Tuple<int, int>(t.Item1 + g.Note(), t.Item2 + 1);
+                    Tuple<int, int> t = cotesforActivity[point.Activity];
+                    cotesforActivity[point.Activity] = new Tuple<int, int>(t.Item1 + point.Note(), t.Item2 + 1);
                 }
                 catch (KeyNotFoundException)
                 {
-                    gradesPerActivity.Add(g.Activity, new Tuple<int, int>(g.Note(), 1));
+                    cotesforActivity.Add(point.Activity, new Tuple<int, int>(point.Note(), 1));
                 }
             }
 
-            // We construct the string to print out, beginning with the name of the student on the first line
+            // To display the name of the student.
             string bulletin = this.Lastname + " " + this.Firstname + "\n";
 
             // And a line for every activity with the code, name, ects and score
-            foreach (KeyValuePair<Activity, Tuple<int, int>> entry in gradesPerActivity)
+            foreach (KeyValuePair<Activity, Tuple<int, int>> entry in cotesforActivity)
             {
                 bulletin += entry.Key.Code + " " + entry.Key.Name + " " + entry.Key.ECTS + " " + entry.Value.Item1 / entry.Value.Item2 + "\n";
             }
