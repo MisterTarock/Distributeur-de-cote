271b98f45ff33fc48f0c2581b9e7a5f47e6dfc5b
Paolo <paolodekeyzer@gmail.com>
2016-10-23 22:23:02 +0200

    Bullentin added

--- Distributeur de cotes/Distributeur de cotes/Program.cs
+++ Distributeur de cotes/Distributeur de cotes/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Distributeur_de_cotes
 {
@@ -10,54 +11,86 @@ namespace Distributeur_de_cotes
     {
         static void Main(string[] args)
         {
-
-            //Encore à améliorer....
-
-           
-             //To define the students
-             Student Puissant = new Student("Puissant", "Victor");
-             Student Petit = new Student("Petit", "Jon");
-
-             //To define the teachers
-             Teacher Combe = new Teacher("Combefis","Sebastien", 2500); //the salary that is a int should not set between ""
-             Teacher Fle = new Teacher("Flemal", "Clemence", 4500);
-             Teacher Lur = new Teacher("Lurquin", "Quentin", 10000);
-
-             //To define the differnete activity
-             Activity COO = new Activity("Conception oriente objet","COO", 3, Combe); // the same for the teacher call by is var
-             Activity EN = new Activity("Electo Numérique", "EN", 3, Fle);
-             Activity PI = new Activity("Projet Info", "PO", 3, Lur);
-
-             //To define the appreciations
-             Appreciation PuissantCOO = new Appreciation(COO,"X");
-             Appreciation PuissantEN = new Appreciation(EN, "");
-             Appreciation PuissantPI = new Appreciation(PI, "TB");
-
-             Appreciation PetitCOO = new Appreciation(COO, "X");
-
-             //The bulletin from Puissant
-             Puissant.Add(PuissantCOO);
-             Puissant.Add(PuissantEN);
-             Puissant.Add(PuissantPI);
-
-             //The bulletin from Petit
-             Petit.Add(PetitCOO);
-
-             Console.WriteLine(Puissant.Bulletin());
-             Console.WriteLine(Puissant.Average());
-
-             Console.WriteLine(Petit.Bulletin());
-             Console.WriteLine(Petit.Average());
-
-             Console.ReadKey(); 
-
-             
+            List<Student> students = ListStudents();
+            List<Teacher> teachers = ListTeachers();
 
 
+            List<Activity> activities = ListActivity();
 
+            readGrades();
 
+            foreach (Student student in students)
+            {
+                File.WriteAllText("../../Bulletin-" + student.Lastname + "-" + student.Firstname + ".txt", student.Bulletin());
+                Console.Write(student.Bulletin());
 
+            }
 
+            Console.ReadKey(); 
+            
+        }
+        public static List<Student> ListStudents()
+        {
+            string[] studs = System.IO.File.ReadAllLines("../../students.csv");
+            List<Student> students = new List<Student>();
+            foreach (string line in studs)
+            {
+                List<string> elems = line.Split(',').Select(elem => elem.Trim()).ToList<string>();
+                students.Add(new Student(elems[0], elems[1]));
+            }
+            return students;
+        }
+        
+        public static List<Teacher> ListTeachers()
+        {
+            string[] teachs = System.IO.File.ReadAllLines("../../Teachers.csv");
+            List<Teacher> teachers = new List<Teacher>();
+            foreach(string line in teachs)
+            {
+                List<string> elems = line.Split(',').Select(elem => elem.Trim()).ToList<string>();
+                teachers.Add(new Teacher(elems[0], elems[1], Int32.Parse(elems[2])));
+            }
+            return teachers;
+        }
+        public static List<Activity> ListActivity()
+        {
+            string[] actis = System.IO.File.ReadAllLines("../../Activities.csv");
+            List<Activity> activities = new List<Activity>();
+            List<Teacher> teachers = ListTeachers();
+            foreach(string line in actis)
+            {
+                List<string> elems = line.Split(',').Select(elem => elem.Trim()).ToList<string>();
+                activities.Add(new Activity(elems[0], elems[1], Int32.Parse(elems[3]),teachers.Find(t => t.Lastname == elems[2])));
+            }
+            return activities;
+        }
+        public static void readGrades( )
+        {
+            string[] grds = System.IO.File.ReadAllLines("../../Cotes.csv");
+            List<Activity> activities = ListActivity();
+            List<Student> students = ListStudents();
+            foreach (string line in grds)
+            {
+                List<string> elems = line.Split(',').Select(elem => elem.Trim()).ToList<string>();
+
+                Evaluation grade;
+
+                // Grades can be either given as an int or a string (N, C, B, ...)
+                // We first try to parse the grade from the file as a number, if that fails (raises an exception)
+                // we assume the grade is in the string fromat
+                try
+                {
+                    grade = new Cote(Int32.Parse(elems[2]),activities.Find(a => a.Code == elems[1]));
+                }
+                catch (FormatException)
+                {
+                    grade = new Appreciation(activities.Find(a => a.Code == elems[1]), elems[2]);
+                }
+
+                // Find the corresponding student and add the grade
+                students.Find(s => s.Lastname == elems[0])
+                        .Add(grade);
+            }
         }
     }
 }
