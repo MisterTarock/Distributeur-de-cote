4e536b72a2aff111df8d4fb1ae25d58ba2425be3
Paolo <paolodekeyzer@gmail.com>
2016-10-25 17:00:47 +0200

    Final

--- Distributeur de cotes/Distributeur de cotes/Student.cs
+++ Distributeur de cotes/Distributeur de cotes/Student.cs
@@ -23,33 +23,47 @@ namespace Distributeur_de_cotes
         //to Add the Eval
         public void Add(Evaluation eval)
         {
-            this.cours.Add(eval);
+            
+            cours.Add(eval);
             
         }
 
         //to make the average of all the eval
-        //right now the calcul of the average is false....
-        public double Average()
+        public string Average()
         {
             var sum = 0;
+            string result = "";
+            
             foreach (var n in this.cours)
 
                 sum += n.Note();
-
-            return sum /this.cours.Count;
+            try { 
+                sum = sum / this.cours.Count;
+                result = "The student average is:";
+                result +=Convert.ToString(sum);
+                return result;
+                
+            }
+            catch(DivideByZeroException)
+            {
+                Convert.ToString(sum);
+                result="Can't do student average: No evalutaions yet";
+                return result;
+            }
+            
         }
 
        
         public string Bulletin()
         { 
             Dictionary<Activity, Tuple<int, int>> cotesforActivity = new Dictionary<Activity, Tuple<int, int>>();
-            Console.WriteLine(this.cours);
-            foreach (Distributeur_de_cotes.Evaluation point in cours){
+            
+            foreach (var point in cours){
                 
 
                 try
                 {
-                    Console.WriteLine("deo");
+                    
                     Tuple<int, int> t = cotesforActivity[point.Activity];
                     cotesforActivity[point.Activity] = new Tuple<int, int>(t.Item1 + point.Note(), t.Item2 + 1);
                 }
@@ -65,11 +79,11 @@ namespace Distributeur_de_cotes
             
             foreach (KeyValuePair<Activity, Tuple<int, int>> entry in cotesforActivity)
             {
-                Console.WriteLine("Test");
+                
                 bulletin += entry.Key.Code + " " + entry.Key.Name + " " + entry.Key.ECTS + " " + entry.Value.Item1 / entry.Value.Item2 + "\n";
             }
 
-            bulletin += "\n\n";
+            
 
             return bulletin;
         }
